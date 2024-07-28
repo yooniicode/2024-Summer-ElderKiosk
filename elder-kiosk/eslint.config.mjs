7796e32a03c0fa1dee7bc77abd58d7d158b5778a
avocado8 <574ple1231@gmail.com>
2024-07-28 15:02:47 +0900

    change dependencies

--- elder-kiosk/eslint.config.mjs
+++ elder-kiosk/eslint.config.mjs
@@ -1,21 +1,28 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import pluginReact from 'eslint-plugin-react'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import someConfig from 'some-other-config-you-use';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = [
+  // Any other config imports go at the top
+  eslintPluginPrettierRecommended,
+];
 
 export default [
-    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
-    {
-        plugins: {
-            prettier: pluginPrettier,
-        },
-        rules: {
-            'prettier/prettier': 'error',
-        },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  someConfig,
+  eslintConfigPrettier,
+  {
+    rules: {
+      semi: 'error',
     },
-    ...prettierConfigs,
-]
+  },
+];
